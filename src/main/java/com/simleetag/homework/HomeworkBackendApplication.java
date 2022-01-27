a231a80d46f77e37d6ed0f0da8110231fd8232c6
Poogle <58318786+suhyunsim@users.noreply.github.com>
2022-01-27 15:37:24 +0900

    프로젝트 생성 및 초기 설정 (#1)

---
+++ src/main/java/com/simleetag/homework/HomeworkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.simleetag.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simleetag/homework/HomeworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simleetag.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

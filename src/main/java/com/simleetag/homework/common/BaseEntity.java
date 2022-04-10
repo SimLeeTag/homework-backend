a221b99773846117da52bb35ae70536873fa41be
Seed <hsik0225@gmail.com>
2022-04-10 17:05:20 +0900

    User 엔티티 설계 (#4)
    * FEAT: 최초 엔티티(User) 설계
    
    * REFACTOR: 생성자 접근제한자 수정

---
+++ src/main/java/com/simleetag/homework/common/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.simleetag.homework.common;
+
+import java.time.LocalDateTime;
+import javax.persistence.*;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @CreatedDate
+    @Column(nullable = false)
+    protected LocalDateTime createdAt;
+
+    protected BaseEntity(Long id, LocalDateTime createdAt) {
+        this.id = id;
+        this.createdAt = createdAt;
+    }
+}

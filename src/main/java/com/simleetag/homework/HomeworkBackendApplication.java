9049ef934006e48d0c9748fdcc27bf077da7ce38
seed <hsik0225@gmail.com>
2022-04-21 18:56:59 +0900

    FEAT: 엔티티 AuditingListener 추가

--- src/main/java/com/simleetag/homework/HomeworkBackendApplication.java
+++ src/main/java/com/simleetag/homework/HomeworkBackendApplication.java
@@ -2,8 +2,11 @@ package com.simleetag.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@ConfigurationPropertiesScan
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HomeworkBackendApplication {
 
     public static void main(String[] args) {

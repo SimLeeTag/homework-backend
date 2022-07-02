78fc15fd519e5dc798ce5906a73f6f51f76c529a
suhyunsim <cnc1106@naver.com>
2022-07-02 20:34:17 +0900

    FEAT: invitation으로 집 멤버 조회 API 구현

---
+++ src/main/java/com/simleetag/homework/api/common/InvitationArgumentResolver.java
@@ -0,0 +1,40 @@
+package com.simleetag.homework.api.common;
+
+import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
+
+import com.simleetag.homework.api.common.exception.AuthenticationException;
+import com.simleetag.homework.api.common.exception.CustomHeaderException;
+import com.simleetag.homework.api.domain.home.infra.HomeJwt;
+import com.simleetag.homework.api.domain.oauth.infra.OAuthJwt;
+import com.simleetag.homework.api.domain.user.LoginUser;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class InvitationArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HomeJwt homeJwt;
+
+    public InvitationArgumentResolver(HomeJwt homeJwt) {
+        this.homeJwt = homeJwt;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Invitation.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        final HttpServletRequest request = Objects.requireNonNull(webRequest.getNativeRequest(HttpServletRequest.class));
+        String invitationToken = request.getHeader("invitation");
+        if (!invitationToken.isEmpty()) {
+            return homeJwt.parseClaimsAsHomeId(invitationToken);
+        }
+        throw new CustomHeaderException("invitation header의 값이 비어있습니다.");
+    }
+}
---
+++ src/main/java/com/simleetag/homework/api/common/exception/CustomHeaderException.java
@@ -0,0 +1,7 @@
+package com.simleetag.homework.api.common.exception;
+
+public class CustomHeaderException extends RuntimeException {
+    public CustomHeaderException(String message) {
+        super(message);
+    }
+}

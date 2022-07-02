eaff2053d2551c48ade8a4dca583d8211e93a070
suhyunsim <cnc1106@naver.com>
2022-07-02 17:25:49 +0900

    FIX: 사용하지 않는 resolver 제거, customExceptionHandler 추가

--- src/main/java/com/simleetag/homework/api/config/WebMvcConfig.java
+++ src/main/java/com/simleetag/homework/api/config/WebMvcConfig.java
@@ -2,9 +2,7 @@ package com.simleetag.homework.api.config;
 
 import java.util.List;
 
-import com.simleetag.homework.api.common.InvitationArgumentResolver;
 import com.simleetag.homework.api.common.LoginUserArgumentResolver;
-import com.simleetag.homework.api.domain.home.infra.HomeJwt;
 import com.simleetag.homework.api.domain.oauth.infra.OAuthJwt;
 
 import org.springframework.context.annotation.Configuration;
@@ -15,16 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final OAuthJwt oauthJwt;
-    private final HomeJwt homeJwt;
 
-    public WebMvcConfig(OAuthJwt oauthJwt, HomeJwt homeJwt) {
+    public WebMvcConfig(OAuthJwt oauthJwt) {
         this.oauthJwt = oauthJwt;
-        this.homeJwt = homeJwt;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUserArgumentResolver(oauthJwt));
-        resolvers.add(new InvitationArgumentResolver(homeJwt));
     }
 }

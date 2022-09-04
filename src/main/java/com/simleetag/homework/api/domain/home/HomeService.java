40e943d6b2eaea4406d027fcb3935f834efb02a8
rowan <hsik0225@gmail.com>
2022-09-04 22:11:07 +0900

    메소드 순서 변경

--- src/main/java/com/simleetag/homework/api/domain/home/HomeService.java
+++ src/main/java/com/simleetag/homework/api/domain/home/HomeService.java
@@ -2,10 +2,7 @@ package com.simleetag.homework.api.domain.home;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-import com.simleetag.homework.api.common.exception.HomeJoinException;
 import com.simleetag.homework.api.domain.home.api.dto.CreateHomeRequest;
 import com.simleetag.homework.api.domain.home.api.dto.CreatedHomeResponse;
 import com.simleetag.homework.api.domain.home.api.dto.HomeWithMembersResponse;
@@ -33,45 +30,9 @@ public class HomeService {
     private final HomeJwt homeJwt;
 
     public List<HomeWithMembersResponse> findAllByMemberIds(List<Long> memberIds) {
-        final List<Long> homeIds = memberService.findAllById(memberIds)
-                                                .stream()
-                                                .map(Member::getHome)
-                                                .map(Home::getId)
-                                                .toList();
+        final List<Long> homeIds = memberService.findAllById(memberIds).stream().map(Member::getHome).map(Home::getId).toList();
 
         return findAllByIdIn(homeIds);
-    }
-
-    public CreatedHomeResponse createHome(Long userId, CreateHomeRequest homeRequest) {
-        final List<Long> memberIds = userService.findById(userId).getMemberIds();
-        if (memberIds.size() >= 3) {
-            throw new HomeJoinException("최대 3개의 집에 소속될 수 있습니다.");
-        }
-
-        // 집 저장
-        final Home home = homeRepository.save(new Home(homeRequest.homeName()));
-
-        // 집 입장
-        Member member = new Member(userId, 0);
-        member.setBy(home);
-        final Long memberId = memberService.save(member).getId();
-        memberIds.add(memberId);
-
-        return CreatedHomeResponse.from(home, homeJwt);
-    }
-
-    public HomeWithMembersResponse findById(Long homeId) {
-        final Home home = findHomeById(homeId);
-        final List<Member> members = home.getMembers();
-        final Set<Long> userIds = members.stream()
-                                         .map(Member::getUserId)
-                                         .collect(Collectors.toSet());
-
-        final List<User> users = userIds.stream()
-                                        .map(userService::findById)
-                                        .toList();
-
-        return HomeWithMembersResponse.from(home, MemberResponse.from(members, users));
     }
 
     public List<HomeWithMembersResponse> findAllByIdIn(List<Long> homeIds) {
@@ -79,8 +40,23 @@ public class HomeService {
     }
 
     private Home findHomeById(Long homeId) {
-        return homeRepository.findById(homeId)
-                             .orElseThrow(() -> new IllegalArgumentException(String.format("HomeID[%d]에 해당하는 집이 존재하지 않습니다.", homeId)));
+        return homeRepository.findById(homeId).orElseThrow(() -> new IllegalArgumentException(String.format("HomeID[%d]에 해당하는 집이 존재하지 않습니다.", homeId)));
+    }
+
+    public HomeWithMembersResponse findById(Long homeId) {
+        final Home home = findHomeById(homeId);
+        final List<Member> members = home.getMembers();
+        final List<User> users = members.stream().map(Member::getUserId).distinct().map(userService::findById).toList();
+
+        return HomeWithMembersResponse.from(home, MemberResponse.from(members, users));
+    }
+
+    public CreatedHomeResponse createHome(Long userId, CreateHomeRequest homeRequest) {
+        final Home home = homeRepository.save(new Home(homeRequest.homeName()));
+
+        memberService.join(home, userId);
+
+        return CreatedHomeResponse.from(home, homeJwt);
     }
 
     public MemberIdResponse joinHome(Long userId, Long homeId) {
@@ -89,19 +65,10 @@ public class HomeService {
             return new MemberIdResponse(joinedMember.get().getId());
         }
 
-        final List<Long> memberIds = userService.findById(userId).getMemberIds();
-        if (memberIds.size() >= 3) {
-            throw new HomeJoinException("최대 3개의 집에 소속될 수 있습니다.");
-        }
-
         Home home = findHomeById(homeId);
 
-        // 집 입장
-        Member member = new Member(userId, 0);
-        member.setBy(home);
-        final Long memberId = memberService.save(member).getId();
-        memberIds.add(memberId);
+        final Member member = memberService.join(home, userId);
 
-        return new MemberIdResponse(memberService.save(member).getId());
+        return new MemberIdResponse(member.getId());
     }
 }

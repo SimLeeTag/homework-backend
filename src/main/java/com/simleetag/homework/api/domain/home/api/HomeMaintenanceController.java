d635daba475263b6c2493557c925051426cc1518
suhyunsim <cnc1106@naver.com>
2023-01-22 23:11:39 +0900

    FIX: 사용하지 않는 코드 정리, 일회성 집안일 샘플 수정

--- src/main/java/com/simleetag/homework/api/domain/home/api/HomeMaintenanceController.java
+++ src/main/java/com/simleetag/homework/api/domain/home/api/HomeMaintenanceController.java
@@ -17,9 +17,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Tag(name = "집 관리")
 @RequiredArgsConstructor
 @RestController
